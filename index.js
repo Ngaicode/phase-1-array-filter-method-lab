d6299b85a099b41e6d9aa6c72b0636fe55787f6f
Ngaicode <mjombakevin376@gmail.com>
2022-06-09 23:29:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+"use strict";
+function findMatching(oneSauce, soughtAfter) {
+  return oneSauce.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === soughtAfter.toLowerCase()
+  );
+}
+function fuzzyMatch(source, testString) {
+  return source.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+function matchName(origin, nameLookedFor) {
+  return origin.filter((record) => record.name === nameLookedFor);
+}
